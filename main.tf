59740ec12415e6dc37dc4d052bf1a4b109c24c4e
arun1267 <acharyarun553@gmail.com>
2023-05-06 20:44:40 +0530

    asdfgh

---
+++ main.tf
@@ -0,0 +1,20 @@
+provider "aws"{
+region ="us-east-1"
+access_key  ="AKIA5KIQROY2UGPWVF4F"
+secret_key ="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"
+}
+resource "aws_instance" "web" {
+ami = "ami-007855ac798b5175e"
+instance_type = "t2.small"
+key_name = "arun"
+tags = {
+Name = "arun123"
+}
+}
+resource "aws_s3_bucket" "venna" {
+bucket = "venna12"
+tags = {
+Name = "master"
+}
+}
+
